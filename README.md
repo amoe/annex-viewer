e0e4278d24ff4f5bac41e2af078546e63e70f767
David Banks <amoebae@gmail.com>
2022-07-20 19:27:58 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,11 @@
+This is a Flask app
+
+## Run
+
+    poetry run flask run
+
+Now you should be able to visit a path under the annex, by using the `annex`
+path prefix.
+
+e.g. http://localhost:5000/annex/cooking/recipes.md
+
---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask
+import markdown
+import os
+import pdb
+
+m = markdown.Markdown()
+
+app = Flask(__name__)
+
+ANNEX_LOCATION = "/home/amoe/ng-annex"
+
+HEADER = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Conforming HTML5 Template</title>
+
+<link rel="shortcut icon" href="data:," type="image/x-icon">
+<style>
+body{
+margin:40px
+auto;max-width:650px;line-height:1.6;font-size:18px;color:#444;padding:0
+10px;
+font-family: serif;
+}h1,h2,h3{line-height:1.2}
+</style>
+</head>
+<body>
+"""
+
+FOOTER = """
+</body>
+</html>
+"""
+
+@app.route('/annex/<path:path>')
+def hello(path):
+    full_path = os.path.join(ANNEX_LOCATION, os.path.normpath(path))
+    with open(full_path, 'r') as f:
+        html = m.convert(f.read())
+    m.reset()
+    return "{}\n{}\n{}".format(HEADER, html, FOOTER)
